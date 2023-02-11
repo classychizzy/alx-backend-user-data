95686add47e2bc663b709d895f304fcefe2602bd
classychizzy <chizobachukwuoma@gmail.com>
2023-02-11 21:39:15 +0000

    task 0

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+a module that handles authentication
+"""
+
+import requests
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    a class that handles authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        a function that defines what requires authentication
+        """
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path in excluded_paths or "{}/".format(path) in excluded_paths:
+            return False
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == "/":
+                continue
+            if path.startswith(excluded_path[:-1]):
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ a function that authorizes the request header
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        else:
+            return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        a function that checks for the instance of the current
+        user
+        """
+        return None
