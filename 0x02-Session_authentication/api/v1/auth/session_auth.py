6783b523889145e1c7da35da8a8dedb52c5430fc
classychizzy <chizobachukwuoma@gmail.com>
2023-02-13 14:11:05 +0000

    task 1

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,4 +7,17 @@ from api.v1.auth.auth import Auth
 class SessionAuth(Auth):
     """ a class that implements session auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ a function that creates a session """
+        if user_id is None:
+            return None
+
+        if type(user_id) is not str:
+            return None
+
+        from uuid import uuid4
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
