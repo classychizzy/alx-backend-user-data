1e654f4fe6753209614f9899fcfe705625a8ec61
classychizzy <chizobachukwuoma@gmail.com>
2023-02-13 21:19:48 +0000

    bug fixes

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -42,13 +42,3 @@ def auth_session_logout():
     if auth.destroy_session(request) is False:
         abort(404)
     return jsonify({}), 200
-
-
-@app_views.route('/auth_session/logout',
-                 methods=['DELETE'], strict_slashes=False)
-def auth_session_logout():
-    """Handles session logout"""
-    from api.v1.app import auth
-    if auth.destroy_session(request) is False:
-        abort(404)
-    return jsonify({}), 200
