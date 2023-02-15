82fb435864235830b38905610059455307f4ef4e
classychizzy <chizobachukwuoma@gmail.com>
2023-02-15 13:42:43 +0000

    bug fix

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,12 +5,14 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
+
 # declarative base class
 Base = declarative_base()
 
 
 # an example mapping using the base
 class User(Base):
+    """ User class """
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
