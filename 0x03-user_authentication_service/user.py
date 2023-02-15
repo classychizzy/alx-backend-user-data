847304db6bdf2c54c285adcea8a1dc737a24fab8
classychizzy <chizobachukwuoma@gmail.com>
2023-02-15 13:11:28 +0000

    task 0 and 1

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" model for a user """
+
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+# declarative base class
+Base = declarative_base()
+
+# an example mapping using the base
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
